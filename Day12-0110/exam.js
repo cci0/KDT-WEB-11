c8e0122573655fd52d5882a53cb66885fea1fa9f
cci0 <dudtidd4484@gmail.com>
2024-01-10 23:46:51 +0900

    study: Day12

--- Day12-0110/exam.js
+++ Day12-0110/exam.js
@@ -1,9 +1,24 @@
 function apple() {
-    $('.box').text('');
+    $('.box').empty();
     $('.box').append(
-        '<img src="https://img.khan.co.kr/news/2023/09/27/l_2023092801001014200092871.jpg" alt="사과"></img>'
+        '<img src="https://img.khan.co.kr/news/2023/09/27/l_2023092801001014200092871.jpg" alt="사과" width="200px" height="200px" text-align:center;></img>'
     );
 }
-function banana() {}
-function grape() {}
-function peach() {}
+function banana() {
+    $('.box').empty();
+    $('.box').append(
+        '<img src="https://health.chosun.com/site/data/img_dir/2023/07/14/2023071401917_0.jpg" alt="바나나" width="200px" height="200px" text-align:center;></img>'
+    );
+}
+function grape() {
+    $('.box').empty();
+    $('.box').append(
+        '<img src="https://img.segye.com/content/image/2017/08/30/20170830515039.jpg" alt="포도" width="200px" height="200px" text-align:center;></img>'
+    );
+}
+function peach() {
+    $('.box').empty();
+    $('.box').append(
+        '<img src="https://www.healthweek.co.kr/boardImage/healthweek/20200827/MC4xMjIyMTIwMCAxNTk4NTE2NjA5.jpeg" alt="복숭아" width="200px" height="200px" text-align:center;></img>'
+    );
+}
