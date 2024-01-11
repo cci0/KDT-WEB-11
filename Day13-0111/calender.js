f01094bca6a14e74d18cb30c882f347d914dfc27
cci0 <dudtidd4484@gmail.com>
2024-01-11 23:46:16 +0900

    study: day13

--- Day13-0111/calender.js
+++ Day13-0111/calender.js
@@ -1,1 +1,16 @@
-function 
+
+$('td').on('click', function(){
+    console.log(this);
+    console.log($(this).text());
+    const selectDate = $(this).tect();
+
+    $('#writeDay').val(selectDate);
+})
+
+function content() {
+    // 작성 날짜(input writeDate)와 내용(input writeContent) 가져오기
+
+    const selectedDate = $('#writeDate').val();
+    const content = $('#writeContent').val();
+
+ }
