fd28ad10d13f41ae698f6698c3bc4cca45159c20
cci0 <dudtidd4484@gmail.com>
2024-01-12 21:11:46 +0900

    study: day14

--- Pratice1-HTML/방명록.js
+++ Pratice1-HTML/방명록.js
@@ -1,6 +1,6 @@
 let i = 2; // 테이블 번호 2번부터 시작해야하기 때문에
 
-function writeFunc(){
+function writeFunc() {
     let writer = document.getElementById('writer').value;
     let content = document.getElementById('writer').value;
     console.log(writer, writer);
@@ -11,17 +11,17 @@ function writeFunc(){
     let tdNum = document.createElement('td'); // 한 열을 제작
     tdNum.textContent = i++; // 증가한다
 
-    let tdWriter = document.createElement('td') // rdWriter 열 제작
+    let tdWriter = document.createElement('td'); // rdWriter 열 제작
     tdWriter.textContent = writer; // 그리고 생성된 셀에 속성을 부여한다.
 
-    let tdContent = document.createElement('td')
+    let tdContent = document.createElement('td');
     tdContent.textContent = content;
 
-    let tdDate = document.createElement('td')
-    let now = new Date()
-    tdDate.textContent = `${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()} 
-    ${now.getHours()}:${now.getMinutes()}`
+    let tdDate = document.createElement('td');
+    let now = new Date();
+    tdDate.textContent = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()} 
+    ${now.getHours()}:${now.getMinutes()}`;
 
-    tr.append(tdNum, tdWriter, tdContent, tdDate) // 순서대로 tr 안에 추가
-    tbody.append(tr) // tbody 안에 tr을 추가
+    tr.append(tdNum, tdWriter, tdContent, tdDate); // 순서대로 tr 안에 추가
+    tbody.append(tr); // tbody 안에 tr을 추가
 }
