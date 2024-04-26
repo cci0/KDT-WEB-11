8162dacb2873eae7fd27f123ffc4a315a634e4e0
cci0 <dudtidd4484@gmail.com>
2024-04-26 13:55:12 +0900

    study: 0425 nextjs

---
+++ practice/src/app/page.js
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+
+export default function Home() {
+    return (
+        <div>
+            <h2>Welcome</h2>
+            Hello, WEB!
+            <br />
+            <br />
+            <img src="/hello.png" alt="hello" />
+        </div>
+    );
+}
---
+++ practice/src/app/read/[id]/page.js
@@ -0,0 +1,8 @@
+export default function Read(props) {
+    return (
+        <div>
+            <h2>Read</h2>
+            paramters : {props.params.id}
+        </div>
+    );
+}
