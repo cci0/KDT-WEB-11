4efd950e62967a76c33cef7dd4ff26c69fc7758f
cci0 <dudtidd4484@gmail.com>
2024-04-27 13:50:24 +0900

    study: 0427 next

--- practice/src/app/create/page.js
+++ practice/src/app/create/page.js
@@ -1,3 +1,35 @@
+'use client';
 export default function Create() {
-    return <div>Create~~~~~~~</div>;
+    return (
+        <form
+            onSubmit={(event) => {
+                event.preventDefault();
+                const title = event.target.title.value;
+                const body = event.target.body.value;
+
+                const options = {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ title, body }),
+                };
+
+                fetch(`http://localhost:9999/topics`, options)
+                    .then((res) => res.json())
+                    .then((result) => {
+                        console.log(result);
+                        const lastid = result.id;
+                    });
+            }}
+        >
+            <p>
+                <input type="text" name="title" placeholder="title" />
+            </p>
+            <p>
+                <textarea name="body" placeholder="body" cols="30" rows="10"></textarea>
+            </p>
+            <input type="submit" value="create" />
+        </form>
+    );
 }
